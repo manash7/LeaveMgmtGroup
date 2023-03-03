337fed22a11d679608fa493eadd08a6c555f1782
manash7 <manashgupta420@gmail.com>
2023-03-03 10:47:37 +0530

    Email And Password Validationadded

--- LeaveManagementAPP/LoginWindow.xaml.cs
+++ LeaveManagementAPP/LoginWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,37 +29,61 @@ namespace LeaveManagementAPP
         private void Login_Click(object sender, RoutedEventArgs e)
         {
             //var Check = context.Employees.Where(e=> e.EmpEmail == textEmail.Text).Select(e=> new { Email = e.EmpEmail }).Contains(e.emp);
-            var data = context.Employees.Where(c => c.EmpEmail == textEmail.Text).FirstOrDefault();
+
 
             //Checking For Null Values 
-            if (textEmail.Text == null || textPassword == null)
+            if (textEmail.Text.Length == 0 )
             {
                 MessageBox.Show("Please Enter Values");
             }
-            else if (textEmail.Text == data.EmpEmail && textPassword.Password == data.EmpPassword)
+            else if ((!Regex.IsMatch(textEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$")))
             {
-                //MessageBox.Show("Login Successful");
-                if (data.Is_SuperUser == false)
-                {
-                    // MessageBox.Show("Not a Super USer");
-                    Window emp = new Employe_dashboard(data);
-                    emp.Show();
-                    Close();
-                }
-                else
-                {
-                    Window admin = new MainWindow();
-                    admin.Show();
-                    Close();
-                }
+                MessageBox.Show("Please Enter Valid Email");
+                textEmail.Focus();
             }
-
+            else if (textPassword.Password.Length == 0)
+            {
+                MessageBox.Show("Please Enter Password");
+            }
+            //else if (textEmail.Text == data.EmpEmail && textPassword.Password == data.EmpPassword)
+            //{
+            //    //MessageBox.Show("Login Successful");
+            //    if (data.Is_SuperUser == false)
+            //    {
+            //        // MessageBox.Show("Not a Super USer");
+            //        Window emp = new Employe_dashboard(data);
+            //        emp.Show();
+            //        Close();
+            //    }
+            //    else
+            //    {
+            //        Window admin = new MainWindow();
+            //        admin.Show();
+            //        Close();
+            //    }
+            //}
             else
             {
-                MessageBox.Show("Invalid Credentials...");
+                var data = context.Employees.Where(c => c.EmpEmail == textEmail.Text).FirstOrDefault();
+
+                if (textEmail.Text == data.EmpEmail && textPassword.Password == data.EmpPassword)
+                {
+                    //MessageBox.Show("Login Successful");
+                    if (data.Is_SuperUser == false)
+                    {
+                        // MessageBox.Show("Not a Super USer");
+                        Window emp = new Employe_dashboard(data);
+                        emp.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        Window admin = new MainWindow();
+                        admin.Show();
+                        Close();
+                    }
+                }
             }
-
-
         }
     }
 }
